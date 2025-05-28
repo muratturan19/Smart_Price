e01df4219e4f4e9afd2a813d019069c56d3b56df
Murat Turan <muratturan19@gmail.com>
2025-05-28 21:01:09 +0300

    Load env variables when launching app

--- run_app.py
+++ run_app.py
@@ -1,6 +1,11 @@
+import os
 import sys
 import pathlib
+from dotenv import load_dotenv
 from streamlit.web import cli as stcli
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".env"))
+load_dotenv(dotenv_path=project_root)
 
 if __name__ == "__main__":
     pkg_root = pathlib.Path(__file__).parent
