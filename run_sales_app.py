b1205dc595216eee9e3be3ed68ce8922b359867d
Murat Turan <muratturan19@gmail.com>
2025-06-08 04:11:17 +0300

    Fix run_sales_app launcher

--- run_sales_app.py
+++ run_sales_app.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 import sys
 from streamlit.web import cli as stcli
@@ -7,4 +6,5 @@ if __name__ == "__main__":
     pkg_root = pathlib.Path(__file__).parent
     sys.path.insert(0, str(pkg_root / "Sales App"))
     st_file = pkg_root / "Sales App" / "sales_app" / "streamlit_app.py"
-    stcli.main(["streamlit", "run", str(st_file)])
+    sys.argv = ["streamlit", "run", str(st_file)]
+    stcli.main()
