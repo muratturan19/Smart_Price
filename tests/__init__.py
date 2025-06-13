e7ed0fd19be790bbf31dbafba6af237057563519
Murat Turan <muratturan19@gmail.com>
2025-06-13 03:15:20 +0300

    Update setuptools config

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,3 @@ from pathlib import Path
 root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(root / "Price App"))
 sys.path.insert(0, str(root / "Sales App"))
-sys.path.insert(0, str(root / "src"))
